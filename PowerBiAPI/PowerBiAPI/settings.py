db4ec2d39b1d35b7888816bcee5c40a48d0bccbf
shahzma <shahzmaalif@gmail.com>
2022-06-04 00:50:47 +0530

    Update settings.py

--- PowerBiAPI/PowerBiAPI/settings.py
+++ PowerBiAPI/PowerBiAPI/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shahzmaalif@gmail.com'
-EMAIL_HOST_PASSWORD = 'sgvzfzbewnuodsas'
+EMAIL_HOST_PASSWORD = ''
